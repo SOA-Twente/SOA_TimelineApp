252ea200c1d641e6233e72c9b642ed956f27219c
Yun <81588081+YunFeng-Zheng@users.noreply.github.com>
2023-03-22 21:54:01 +0100

    Add files via upload

---
+++ TimelineApp/src/main/java/com/QuackAttack/TimelineApp/TimelineAppApplication.java
@@ -0,0 +1,13 @@
+package com.QuackAttack.TimelineApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimelineAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimelineAppApplication.class, args);
+	}
+
+}
---
+++ TimelineApp/src/test/java/com/QuackAttack/TimelineApp/TimelineAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.QuackAttack.TimelineApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimelineAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
